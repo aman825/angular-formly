6d4d145fa3565361c6fc4efdfba90b7e531b205e
aman82500 <amanambar@gmail.com>
2021-10-18 17:28:50 +0530

    first leg done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {FormlyFieldConfig} from '@ngx-formly/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,93 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  form = new FormGroup({});
+  model=  {email: 'email@gmail.com'}
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'First Name',
+      type: 'input',
+      templateOptions: {
+        label: 'Name',
+        placeholder: 'Enter name',
+        required: true,
+      }
+    },
+    { 
+      key: 'Last Name', 
+      type: 'input', 
+      templateOptions: {
+         label: 'Last Name', 
+         placeholder: 'Last Name',
+         required: true,
+      }
+    },
+    {
+      key: 'Comments- Text Area',
+      type: 'input',
+      templateOptions: {
+              type: 'number',
+        label: 'Comments- Text Area',
+        placeholder: 'Enter amount',
+      }
+    },
+    {
+      key: 'date_of_birth',
+      type: 'datepicker',
+          templateOptions: {
+        label: 'Datepicker',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+      },
+    },
+    {
+      key: 'terms',
+          type: 'checkbox',
+      templateOptions: {
+        label: 'Accept terms',
+        description: 'This is a checkbox',
+        required: true,
+      },
+    },
+    {
+      key: 'terms',
+      type: 'toggle',
+      templateOptions: {
+        label: 'Accept terms',
+        description: 'Do you like options?',
+        required: true,
+      },
+    },
+    {
+      key: 'description',
+      type: 'textarea',
+      templateOptions: {
+        label: 'Description',
+        placeholder: 'Enter description',
+      }
+    },
+    {
+      key: 'multiselect',
+      type: 'select',
+      templateOptions: {
+        label: 'Multi-select',
+        multiple: true,
+        options: [
+          {label: 'Iron Man', value: 'iron_man'},
+          {label: 'Captain America', value: 'captain_america'},
+          {label: 'Black Widow', value: 'black_widow'},
+          {label: 'Hulk', value: 'hulk'},
+          {label: 'Captain Marvel', value: 'captain_marvel'},
+        ],
+      },
+    },
+  
+  ]
+
+  onSubmit(model: any){
+    console.log(model);
+    
+  }
   title = 'formly-poc';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ReactiveFormsModule } from '@angular/forms'
+import { FormlyModule } from '@ngx-formly/core'
+import { FormlyMaterialModule } from '@ngx-formly/material'
+import { FormlyMatDatepickerModule } from '@ngx-formly/material/datepicker';
+import { FormlyMatToggleModule } from '@ngx-formly/material/toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatRadioModule } from '@angular/material/radio'
+import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatNativeDateModule } from '@angular/material/core'
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatCheckboxModule,
+        MatButtonModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        
+        MatNativeDateModule,
+        FormlyMatDatepickerModule,
+        FormlyMatToggleModule,
+        FormlyModule.forRoot(),
+        FormlyMaterialModule,
+        FormlyModule.forRoot({ extras: { lazyRender: true } }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
