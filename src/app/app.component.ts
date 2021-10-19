67eb7333564c6c435cc10513a89ec62c89b5b3c3
aman82500 <amanambar@gmail.com>
2021-10-19 14:41:53 +0530

    dataservice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
-import {FormGroup} from '@angular/forms';
-import {FormlyFieldConfig} from '@ngx-formly/core'
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import {
+  distinctUntilChanged,
+  map,
+  startWith,
+  switchMap,
+} from 'rxjs/operators';
+import { DataService } from './data-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   form = new FormGroup({});
-  model=  {email: 'email@gmail.com'}
+  model: any = {
+    nationId: 1,
+    cityId: 1
+  };
+
   fields: FormlyFieldConfig[] = [
     {
       key: 'First Name',
@@ -18,30 +29,30 @@ export class AppComponent {
         label: 'Name',
         placeholder: 'Enter name',
         required: true,
-      }
+      },
     },
-    { 
-      key: 'Last Name', 
-      type: 'input', 
+    {
+      key: 'Last Name',
+      type: 'input',
       templateOptions: {
-         label: 'Last Name', 
-         placeholder: 'Last Name',
-         required: true,
-      }
+        label: 'Last Name',
+        placeholder: 'Last Name',
+        required: true,
+      },
     },
     {
       key: 'Comments- Text Area',
       type: 'input',
       templateOptions: {
-              type: 'number',
+        type: 'number',
         label: 'Comments- Text Area',
         placeholder: 'Enter amount',
-      }
+      },
     },
     {
       key: 'date_of_birth',
       type: 'datepicker',
-          templateOptions: {
+      templateOptions: {
         label: 'Datepicker',
         placeholder: 'Placeholder',
         description: 'Description',
@@ -50,13 +61,14 @@ export class AppComponent {
     },
     {
       key: 'terms',
-          type: 'checkbox',
+      type: 'checkbox',
       templateOptions: {
         label: 'Accept terms',
         description: 'This is a checkbox',
         required: true,
       },
     },
+
     {
       key: 'terms',
       type: 'toggle',
@@ -72,7 +84,7 @@ export class AppComponent {
       templateOptions: {
         label: 'Description',
         placeholder: 'Enter description',
-      }
+      },
     },
     {
       key: 'multiselect',
@@ -81,20 +93,58 @@ export class AppComponent {
         label: 'Multi-select',
         multiple: true,
         options: [
-          {label: 'Iron Man', value: 'iron_man'},
-          {label: 'Captain America', value: 'captain_america'},
-          {label: 'Black Widow', value: 'black_widow'},
-          {label: 'Hulk', value: 'hulk'},
-          {label: 'Captain Marvel', value: 'captain_marvel'},
+          { label: 'Iron Man', value: 'iron_man' },
+          { label: 'Captain America', value: 'captain_america' },
+          { label: 'Black Widow', value: 'black_widow' },
+          { label: 'Hulk', value: 'hulk' },
+          { label: 'Captain Marvel', value: 'captain_marvel' },
         ],
       },
     },
-  
-  ]
+    {
+      key: 'nationId',
+      type: 'select', // <select>
+      templateOptions: {
+        label: 'Nation',
+        options: this.dataService.getNations(),
+      },
+    },
+    {
+      key: 'cityId',
+      type: 'select', // <select>
+      templateOptions: {
+        label: 'Cities',
+        options: [],
+      },
+      hooks: {
+        onInit: (field?: FormlyFieldConfig) => {
+          console.log(field);
+          if (field && field.templateOptions) {
+            console.log(field);
+            field: {
 
-  onSubmit(model: any){
-    console.log(model);
-    
+              templateOptions: {
+              
+              options:   field?.form?.get('nationId')?.valueChanges?.pipe(
+                  startWith(this.model.nationId),
+                  switchMap((nationId) =>
+                    this.dataService.getCities(nationId)
+                  )
+                )
+              
+              
+              }
+              
+              }
+            
+          }
+        },
+      },
+    },
+  ];
+  constructor(private dataService: DataService) {}
+  onSubmit() {
+    alert(JSON.stringify(this.model));
   }
   title = 'formly-poc';
 }
---
+++ src/app/data-service.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  constructor() {}
+  
+  getNations() {
+    return of([
+      {
+        value: null,
+        label: ' -- '
+      },
+      {
+        value: 1,
+        label: 'Italy'
+      },
+      {
+        value: 2,
+        label: 'Germany'
+      },
+      {
+        value: 3,
+        label: 'U.S.'
+      }
+    ]);
+  }
+
+  getCities(nationId: number = 0) {
+    console.log(nationId);
+    return of(
+      [
+        {
+          value: null,
+          label: ' -- ',
+          nationId: null
+        },
+        {
+          value: 1,
+          label: 'Bolzano',
+          nationId: 1
+        },
+        {
+          value: 12,
+          label: 'Rome',
+          nationId: 1
+        },
+        {
+          value: 2,
+          label: 'Berlin',
+          nationId: 2
+        },
+        {
+          value: 21,
+          label: 'Munich',
+          nationId: 2
+        },
+        {
+          value: 3,
+          label: 'San Francisco',
+          nationId: 3
+        }
+      ].filter(entry => {
+        if (nationId) {
+          return entry.nationId === nationId;
+        } else {
+          return true;
+        }
+      })
+    );
+  }
+}
